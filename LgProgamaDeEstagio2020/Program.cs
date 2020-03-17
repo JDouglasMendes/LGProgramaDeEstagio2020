3d7fd9e3d6de66b2c2d75c73dd110fd5fd0ff956
SCMatheus <matheusouzacardoso2016@gmail.com>
2020-03-17 10:31:54 -0300

    Estagio

---
+++ LgProgamaDeEstagio2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LgProgamaDeEstagio2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
