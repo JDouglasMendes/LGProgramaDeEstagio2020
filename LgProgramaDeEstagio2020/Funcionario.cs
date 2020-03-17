c2d7296b63da7b62fe22358731bb7dba53b3657f
SCMatheus <matheusouzacardoso2016@gmail.com>
2020-03-17 10:57:35 -0300

    Estagio Matheus

---
+++ LgProgramaDeEstagio2020/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LgProgramaDeEstagio2020
+{
+    class Funcionario
+    {
+        private string matricula;
+        private string nome;
+        private double salario;
+        private DateTime dataAdmi;
+        private char tipoFunc; // c - CLT, a - autonomo, p - pro-labore
+        private string cargo;
+
+    }
+}
