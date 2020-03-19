937a5b60b22e67364854ac24c7d9a3567aa749d5
fernandodsb <62293172+fernandodsb@users.noreply.github.com>
2020-03-19 09:08:26 -0300

    Adicionado Interface Folha de Pagamento

---
+++ LgProgramaDeEstagio2020/ICalculoFolhaDePagamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LgProgramaDeEstagio2020
+{
+    public interface ICalculoFolhaDePagamento
+    {
+        double CalcularFolha(Funcionario funcionario, Referencia referencia);
+        
+
+    }
+}
---
+++ LgProgramaDeEstagio2020/Referencia.cs
@@ -0,0 +1,17 @@
+﻿namespace LgProgramaDeEstagio2020
+{
+    public class Referencia
+    {
+       
+        public int Mes { get; private set; }
+        public int Ano { get;} //É TEM A MESMA FUNÇÃO DO DE CIMA
+
+        public Referencia(int mes, int ano)
+        {
+            Mes = mes;
+            Ano = ano;
+        }
+
+
+    }
+}
