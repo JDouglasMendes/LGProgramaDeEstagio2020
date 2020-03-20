1a90f66b89a9847d09fc7f0999fb1a4a218ec33b
SCMatheus <matheusouzacardoso2016@gmail.com>
2020-03-20 16:18:07 -0300

    Corrigindo Classe CalculoSalarioIntermitente

--- LgProgramaDeEstagio2020/CalculoDeSalario/CalculoSalarioIntermitente.cs
+++ LgProgramaDeEstagio2020/CalculoDeSalario/CalculoSalarioIntermitente.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace LgProgramaDeEstagio2020
 {
-    class CalculoSalarioIntermitente : ICalculoFolhaDePagamento<Intermitente>
+     public class CalculoSalarioIntermitente : ICalculoFolhaDePagamento<Intermitente>
     {
-        double ICalculoFolhaDePagamento<Intermitente>.CalcularFolha(Intermitente funcionario, Referencia referencia)
+        public double CalcularFolha(Intermitente funcionario, Referencia referencia)
         {
             return ((funcionario.Salario / DateTime.DaysInMonth(referencia.Ano, referencia.Mes))
                 * referencia.ObtenhaDiasUteisNoMes() + (funcionario.Salario * 0.33) / 12);
