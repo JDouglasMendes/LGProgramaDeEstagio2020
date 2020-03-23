1af3defe68b5daf478ef306e16c63c128131ad11
lipegemmal <lipegemmal@hotmail.com>
2020-03-23 10:40:46 -0300

    Finalizando Relatorios

---
+++ LgProgramaDeEstagio2020/Relatorios/DadosRelatorioDeFolhaDePagamento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LgProgramaDeEstagio2020.Relatorios
+{
+    class DadosRelatorioDeFolhaDePagamento
+    {
+    
+        public string Matricula { get; private set;}
+
+        public string Nome { get; private set; }
+
+        public double Valor { get; private set; }
+
+        public DadosRelatorioDeFolhaDePagamento(string matricula, string nome, double valor)
+        {
+            Matricula = matricula;
+            Nome = nome;
+            Valor = valor;
+        }
+
+        public override string ToString()
+        {
+            return $"Matricula: {Matricula} Nome: {Nome} Valor: {Valor.ToString()}";
+        }
+
+
+    }
+}
---
+++ LgProgramaDeEstagio2020/Relatorios/RelatorioDeFolhaDePagamento.cs
@@ -0,0 +1,37 @@
+﻿using LgProgramaDeEstagio2020.BancoDeDados;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LgProgramaDeEstagio2020.Relatorios
+{
+    class RelatorioDeFolhaDePagamento
+    {
+        private List<DadosRelatorioDeFolhaDePagamento> BuscarDados()
+        {
+            var listaDeTabelasDeFuncionarios = BancoDeDadosEmMemoria<TabelaDeFuncionarios>.Singleton.Select();
+            var listaDeTabelasValoresCalculados = BancoDeDadosEmMemoria<TabelaValoresCalculados>.Singleton.Select();
+
+            var listaDeDadosDaFolha = new List<DadosRelatorioDeFolhaDePagamento>();
+            foreach (var linha in listaDeTabelasValoresCalculados)
+            {
+                listaDeDadosDaFolha.Add(new DadosRelatorioDeFolhaDePagamento(linha.TransformeEmTabela().Matricula,
+                    listaDeTabelasDeFuncionarios.First(x => x.TransformeEmTabela().Matricula == linha.TransformeEmTabela().Matricula).TransformeEmTabela().Nome,
+                    linha.TransformeEmTabela().Valor));
+       
+            }
+            return listaDeDadosDaFolha.OrderBy(x => x.Matricula).ToList();
+        }
+
+        public void ExibirRelatorioDeAdmissoes()
+        {
+            BuscarDados().ForEach(EscrevaLinha);
+        }
+
+        private void EscrevaLinha(DadosRelatorioDeFolhaDePagamento obj)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
