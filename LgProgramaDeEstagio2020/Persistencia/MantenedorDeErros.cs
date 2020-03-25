30d59c5241db6c4326ac1ae9940b4040c5bf1961
lipegemmal <lipegemmal@hotmail.com>
2020-03-25 09:55:00 -0300

    Persistencia de Erros

---
+++ LgProgramaDeEstagio2020/Persistencia/MantenedorDeErros.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace LgProgramaDeEstagio2020.Persistencia
+{
+    public class MantenedorDeErros
+    {
+        public void GravarErros(List<string> listaErros){
+            var info = Path.Combine((new FileInfo(Assembly.GetExecutingAssembly().Location)).FullName, "\\Erros.txt");
+
+            if (! File.Exists(info))        
+                File.Create(info);
+
+            using (var arquivo = new StreamWriter(info, true))
+            {
+                listaErros.ForEach(arquivo.WriteLine);
+            }
+        }
+    }
+}
